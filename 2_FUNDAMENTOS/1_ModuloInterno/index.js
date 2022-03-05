c60c7a879ef139f46b98fad66daa80c84b6795b8
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2022-03-04 22:14:33 -0300

    Exportação e Importação

---
+++ 2_FUNDAMENTOS/1_ModuloInterno/index.js
@@ -0,0 +1,5 @@
+const modulo = require('./modulo');
+const soma = modulo.soma;
+
+console.log(modulo.soma(5,10))
+console.log(soma(10,15))
---
+++ 2_FUNDAMENTOS/2_ExportImport/index.js
@@ -0,0 +1,4 @@
+const modulo = require("../1_ModuloInterno/modulo");
+
+console.log(modulo.soma(10,10));
+console.log(modulo.soma(10,20));
